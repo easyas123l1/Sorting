fd5fa635f4ce22ad331f0d82f40e522c5635ad2c
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-17 13:06:10 -0500

    comment linear and binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -3,6 +3,7 @@ def linear_search(arr, target):
   
   # TO-DO: add missing code
   for i in range(len(arr)):
+    # iterate thru for loop to find target
     if arr[i] == target:
       return i
 
@@ -12,23 +13,26 @@ def linear_search(arr, target):
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
-
+  #edge case can't be used on empty arrays
   if len(arr) == 0:
     return -1 # array empty
-    
+  # low and high value defaults to 0 and last index
   low = 0
   high = len(arr)-1
 
-  # TO-DO: add missing code
+  # default found to False
   found = False
 
+  # while low <= and we haven't found the target keep searching!
   while low <= high and not found:
     #find middle using division
     middle = (low + high) // 2
 
+    # if we find the target return it!
     if arr[middle] == target:
       return middle
 
+    # if we haven't found the target decide lower or higher and change low or high for next search
     else:
       if target <arr[middle]:
         # search lower half
